28334680fafecacdddf2f164d9c50d7b0803fa27
vaishali-agr13 <vaishali.agr13@gmail.com>
2026-05-30 16:06:39 +0530

    remove cache

--- storage/framework/views/853d72299c157ef45b2bf3a1c380a1e8.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer text-center">
-
-    <strong>Copyright &copy; 2026</strong>
-
-</footer><?php /**PATH C:\xampp\htdocs\htdocs\vrinda-taxi\resources\views/admin/layouts/footer.blade.php ENDPATH**/ ?>
